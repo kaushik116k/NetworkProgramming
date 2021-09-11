4e1530f050dc41a4bb43aef3d9daf80a9604ef92
kaush <kaushik116k@gmail.com>
2021-09-11 21:56:12 +0530

    copy the data

--- src/IO_Streams/Copy.java
+++ src/IO_Streams/Copy.java
@@ -1,11 +1,18 @@
 package IO_Streams;
 import java.io.*;
+import java.util.Scanner;
+
 public class Copy {
     // copy the data from one file to another file
     public static void main(String[] args) throws FileNotFoundException, IOException{
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the source file : ");
+        String srcFile = sc.nextLine();
+        System.out.println("Enter the destination file : ");
+        String desFile = sc.nextLine();
         //read the file
-        FileInputStream fis = new FileInputStream("D:\\javaprograms\\Input.txt");
-        FileOutputStream fos = new FileOutputStream("D:\\javaprograms\\Output.txt");
+        FileInputStream fis = new FileInputStream(srcFile);
+        FileOutputStream fos = new FileOutputStream(desFile);
         int data;
         while((data = fis.read()) != -1){
             fos.write((char)data);
